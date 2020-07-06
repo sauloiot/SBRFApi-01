fc7e291121206c48bc55ac40784bbcfcd149d1db
Saulo Tavares <saulo.11@hotmail.com>
2020-07-06 01:26:20 -0300

    working with mysql

--- src/main/java/com/training/restfullcrud/repository/EmployeeRepository.java
+++ src/main/java/com/training/restfullcrud/repository/EmployeeRepository.java
@@ -3,5 +3,6 @@ package com.training.restfullcrud.repository;
 import com.training.restfullcrud.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
